a00cb53966c9f54662b484970712d569383d8639
Yarnballer494 <flowercat@tuta.com>
2026-02-09 23:53:54 +0100

    First commit

---
+++ configuration.nix
@@ -0,0 +1,92 @@
+{ config, lib, pkgs, ... }:
+
+{
+
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "nixos";
+  networking.networkmanager.enable = true;
+
+  time.timeZone = "Europe/Berlin";
+
+  services.getty.autologinUser = "yarn";
+
+  programs.hyprland = {
+    enable = true;
+    xwayland.enable = true;		
+  };
+
+  services.greetd = {
+    enable = true;
+    settings = {
+      default_session = {
+        command = lib.mkDefault "${pkgs.tuigreet}/bin/tuigreet --time --cmd 'systemd-run --user --scope --unit=hyprland hyprland' --user-menu --user-menu-min-uid 1000";
+        user = "yarn";
+      };
+    };
+  };
+
+  services.xserver = {
+    xkb.layout = "de";
+  };
+
+  #Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  #Enable sound.
+  
+
+  #Enable touchpad support (enabled default in most desktopManager).
+  services.libinput.enable = true;
+
+  #Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.yarn = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+    packages = with pkgs; [
+      tree
+    ];
+  };
+
+  programs.firefox.enable = true;
+
+  #List packages installed in system profile.
+  #You can use https://search.nixos.org/ to find more packages (and options).
+  environment.systemPackages = with pkgs; [
+    vim 
+    wget
+    git
+    kitty
+    waybar
+    hyprpaper
+  ];
+
+  fonts.packages = with pkgs; [
+    nerd-fonts.jetbrains-mono  
+  ];
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  system.stateVersion = "25.11";
+
+  nix.gc.automatic = true;
+  nix.gc.dates = "20:00";
+  nix.gc.options = "--delete-older-than 2d";
+
+
+  system.autoUpgrade = {
+    enable = true;
+    flake = "/etc/nixos/flake.nix";
+    flags = [
+      "-L" # Print build logs
+    ];
+    dates = "02:00";
+    randomizedDelaySec = "45min";
+  };
+}
+
---
+++ home.nix
@@ -0,0 +1,54 @@
+{ config, pkgs, ... }:
+
+{
+	home.username = "yarn";
+	home.homeDirectory = "/home/yarn";
+	home.stateVersion = "25.11";
+	programs.bash = {
+		enable = true;
+		shellAliases = {
+			meow = "echo meoww";
+		};
+		profileExtra = ''
+			if [ -z "$WAYLAND_DISPLAY" ] && [ "$XDG_VTNR" = 1 ]; then
+				exec hyprland
+			fi
+		'';
+	};
+
+	programs.git = {
+		enable = true;
+		userName = "Yarnballer494";
+		userEmail = "flowercat@tuta.com";
+		extraConfig = {
+			init.defaultBranch = "main";
+			safe.directory = "/etc/nixos";
+			safe.directory = "/home/yarn/.dotfiles";
+		};
+	};
+
+	wayland.windowManager.hyprland = {
+		enable = true;
+		settings = {
+			"$mod" = "SUPER";
+
+			input = {
+				kb_layout = "de";
+				kb_options = "grp:win_space_toggle";
+			}
+			bind = [
+				# Main binds
+				"$mod, T, exec, kitty"
+				"$mod, C, killactive,"
+				"$mod, M, exec, command -v hyprshutdown >/dev/null 2>&1 && hyprshutdown || hyprctl dispatch exit"
+
+				# Move focus 
+				"$mod, left, movefocus, l"
+				"$mod, right, movefocus, r"
+				"$mod, up, movefocus, u"
+				"$mod, down, movefocus, d"
+			];
+			
+		};
+	};
+}
