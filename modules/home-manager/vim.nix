8ff78306bac13f15d75c6b2152a120f3dd049733
yarn <152090555+Yarnballer494@users.noreply.github.com>
2026-03-19 15:57:14 +0100

    Vim config syntax

--- modules/home-manager/vim.nix
+++ modules/home-manager/vim.nix
@@ -15,9 +15,9 @@ in
                set number
                set relativenumber
                set expandtab
-               set shiftwidth = 4
-               set softtabstop = 4
-               set tabstop = 4
+               set shiftwidth=4
+               set softtabstop=4
+               set tabstop=4
                set smartindent
                syntax on
            '';
