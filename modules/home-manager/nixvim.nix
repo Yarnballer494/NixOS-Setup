145723a458beee740e64e2d585e6b68bc9f33aa8
yarn <152090555+Yarnballer494@users.noreply.github.com>
2026-03-22 03:39:38 +0100

    Nvim clipboard copy paste keymaps

--- modules/home-manager/nixvim.nix
+++ modules/home-manager/nixvim.nix
@@ -13,8 +13,29 @@ in
   config = lib.mkIf cfg.enable {
     programs.nixvim = {
       enable = true;
+      defaultEditor = true;
       colorschemes.catppuccin.enable = true;
       plugins.lualine.enable = true;
+      opts = {
+        number = true;
+	relativenumber = true;
+	shiftwidth = 4;
+      };
+      keymaps = [
+      # Trivial remaps but shows the syntax :)
+	{
+	  mode = ["n" "v"];
+	  key = "<leader>y"; 
+	  options.silent = true;
+	  action = "\"+y";
+	}
+	{
+	  mode = ["n" "v"];
+	  key = "<leader>p";
+	  options.silent = true;
+	  action = "\"+p";
+	}
+      ];
     };   
   };
 }
