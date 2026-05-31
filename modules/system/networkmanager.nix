fffc90a51ddbb6822b02b93dc2ecbb7d8ab10045
yarn <152090555+Yarnballer494@users.noreply.github.com>
2026-05-31 12:01:24 +0200

    Add new wifi

--- modules/system/networkmanager.nix
+++ modules/system/networkmanager.nix
@@ -15,6 +15,8 @@ in
     sops.secrets."wifi/stud/psk" = {};
     sops.secrets."wifi/unic/ssid" = {};
     sops.secrets."wifi/unic/psk" = {};
+    sops.secrets."wifi/home/ssid" = {};
+    sops.secrets."wifi/home/psk" = {};
 
     networking.networkmanager = {
       enable = true;
@@ -27,6 +29,8 @@ in
 	        config.sops.secrets."wifi/stud/psk".path
           config.sops.secrets."wifi/unic/ssid".path
           config.sops.secrets."wifi/unic/psk".path
+          config.sops.secrets."wifi/home/ssid".path
+          config.sops.secrets."wifi/home/psk".path
         ];
 
 	      profiles = {
@@ -82,11 +86,11 @@ in
             };
             wifi = {
               mode = "infrastructure";
-              ssid = "UNIC_SSID";
+              ssid = "$UNIC_SSID";
             };
             wifi-security = {
               key-mgmt = "wpa-psk";
-              psk = "$STUD_PSK";
+              psk = "$UNIC_PSK";
             };
             ipv4 = {
               method = "auto";
@@ -94,6 +98,28 @@ in
             ipv6 = {
              method = "auto";
              addr-gen-mode = "stable-privacy";
+            };
+          };
+          "home" = {
+            connection = {
+              id = "home";
+              type = "wifi";
+              autoconnect = true;
+            };
+            wifi = {
+              mode = "infrastructure";
+              ssid = "$HOME_SSID";
+            };
+            wifi-security = {
+              key-mgmt = "wpa-psk";
+              psk = "$HOME_PSK";
+            };
+            ipv4 = {
+              method = "auto";
+            };
+            ipv6 = {
+              method = "auto";
+              addr-gen-mode = "stable-privacy";
             };
           };
 	      };
